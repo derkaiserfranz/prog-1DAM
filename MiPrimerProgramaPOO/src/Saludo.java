c67d4dc9dbba90c4180470ccf3545a525806292d
unknown <amacias@AULA07-22.aulas.triana>
2014-11-11 14:29:06 +0100

    MiPrimerProgramaPOO

---
+++ MiPrimerProgramaPOO/src/Saludo.java
@@ -0,0 +1,16 @@
+
+public class Saludo {
+
+	// Atributos
+	
+	String hola = "Hola Mundo";
+	
+	// Métodos
+	
+	  //mostrarMensaje();
+	
+	public Saludo (String hola) {
+		this.hola = hola;
+	}
+	
+}
